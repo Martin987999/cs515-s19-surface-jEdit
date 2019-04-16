5238a3c1b962728a2de7191e3fb8ddb41ba716de
Martin987999 <31902538+Martin987999@users.noreply.github.com>
2019-04-16 04:48:45 -0600

    a4_refactoring

--- org/gjt/sp/jedit/RectSelectChanged.java
+++ org/gjt/sp/jedit/RectSelectChanged.java
@@ -1,1 +1,12 @@
+package org.gjt.sp.jedit;
 
+
+import org.gjt.sp.jedit.gui.StatusBar;
+
+
+public class RectSelectChanged extends Flag {
+	public void statusChanged(boolean value, StatusBar status) {
+		status.setMessageAndClear(
+				jEdit.getProperty("view.status.rect-select-changed", new Integer[] { value ? 1 : 0 }));
+	}
+}
